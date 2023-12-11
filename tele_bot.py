f668cfc9ecd8b6bacc9fd59ee95ac77df7e0b25f
Abbas moradi <en.moradi.66@gmail.com>
2023-12-11 20:43:31 +0330

    user register implementation

--- tele_bot.py
+++ tele_bot.py
@@ -1,28 +1,31 @@
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot('6547851672:AAF46rU-DYL6obqQJtB60ZS2EqbFxzUG-HM')
 
-Introducing = telebot.types.InlineKeyboardButton("سازنده ربات", url="https://github.com/Abbas-moradi")
-tejarat = telebot.types.InlineKeyboardButton("بانک تجارت", url="https://tejaratbank.ir")
-markup = telebot.types.InlineKeyboardMarkup()
-markup.add(Introducing, tejarat)
-
 
 @bot.message_handler(commands=['start'])
-def get_number(message):
-    bot.send_message(message.chat.id, " سلام ، به ربات کمک یار کارمند بانک تجارت خوش آمدید."
+def register(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    button = types.KeyboardButton(text='ثبت نام در ربات', request_contact=True)
+    markup.add(button)
+
+    bot.send_message(message.chat.id, "\n سلام ، به ربات کمک یار کارمند بانک تجارت خوش آمدید."
                                     " قصد داریم با این ربات کمی از مشکلات کارمندان بکاهیم"
                                     " شما میتوانید مشکلات خود را بصورت سوال از ربات پرسیده  "
-                                    "و در صورت وجود جواب در داده های ربات ،پاسخ نمایش داده میشود", 
+                                    "و در صورت وجود جواب در داده های ربات ،پاسخ نمایش داده میشود"
+                                    "\n قبل از استفاده از ربات شما باید ثبت نام کنید و اطلاعات شما"
+                                    "در پایگاه داده ی ما ثبت شود",
                      reply_markup=markup)
+    
 
-@bot.message_handler(commands=['help'])
-def help_message(message):
-    bot.reply_to(message, "چجوری میتونم کمکت کنم؟؟؟")
+@bot.message_handler(content_types=['contact'])
+def contact_register(message):
+    print(message.contact)
 
 @bot.message_handler()
-def help_message(message):
+def hello_message(message):
     if message.text in ['سلام', 'hi', 'hello', 'Hi', 'Hello', 'salam', 'چطوری']:
         bot.reply_to(message, "سلام به تو کارمند پرتلاش بانک تجارت")
 
-bot.infinity_polling()
+bot.infinity_polling(none_stop=True)
