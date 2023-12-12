e7d3e72194fbad1ac7cfaca566ce8631bfc2ac1c
Abbas moradi <en.moradi.66@gmail.com>
2023-12-13 00:22:14 +0330

    Implementation of smart search engine using numpy,gensim library for testing

---
+++ AI_query.py
@@ -0,0 +1,38 @@
+from gensim.models import Word2Vec
+
+data = [
+    {"question": "چطور اطلاعات حساب کاربری را تغییر دهم؟", "answer": "برای تغییر اطلاعات حساب کاربری به قسمت تنظیمات مراجعه کنید."},
+    {"question": "سلام چطور می‌توانم به شما کمک کنم؟", "answer": "سلام! خوش آمدید. لطفاً سوال خود را بپرسید."},
+]
+
+# تبدیل سوالات به لیست لغتنامه‌ای
+sentences = [question["question"].split() for question in data]
+
+# ایجاد مدل Word2Vec
+model = Word2Vec(sentences, vector_size=100, window=5, min_count=1, workers=4)
+
+
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+def get_most_similar_question(input_text):
+    # تبدیل متن ورودی به بردار با استفاده از مدل Word2Vec
+    input_vector = np.mean([model.wv[word] for word in input_text.split() if word in model.wv], axis=0)
+
+    # محاسبه شباهت کسینوسی بین متن ورودی و سوالات موجود
+    similarities = [cosine_similarity([input_vector], [model.wv[word] for word in question["question"].split() if word in model.wv])[0][0] for question in data]
+
+    # پیدا کردن سوال با بیشترین شباهت
+    most_similar_index = np.argmax(similarities)
+    most_similar_question = data[most_similar_index]["question"]
+    most_similar_answer = data[most_similar_index]["answer"]
+
+    return most_similar_question, most_similar_answer
+
+
+user_input = "کمک"
+most_similar_question, most_similar_answer = get_most_similar_question(user_input)
+
+print(f"سوال شما: {user_input}")
+print(f"سوال مشابه: {most_similar_question}")
+print(f"پاسخ: {most_similar_answer}")
